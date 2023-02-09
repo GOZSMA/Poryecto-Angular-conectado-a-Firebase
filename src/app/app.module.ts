c890ec7ddea0cfb12cd3d6d3264f98cf982dec01
Brandon Emanuel Gonzalez Morales <brandonnemmanuel@hotmail.com>
2023-02-09 11:43:25 -0600

    Proyecto conectado a FireBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NewPlaceComponent } from './components/new-place/new-place.component';
+import { PlaceListComponent } from './components/place-list/place-list.component';
+import { PlaceMapComponent } from './components/place-map/place-map.component';
+import { AgmCoreModule } from '@agm/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewPlaceComponent,
+    PlaceListComponent,
+    PlaceMapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgbModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'API KEY GOOGLE MAPS'
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'my-project-firebase-94bad',
+    appId: '1:408846258230:web:00c294afba1f73d18a8897',
+    storageBucket: 'my-project-firebase-94bad.appspot.com',
+    apiKey: 'AIzaSyAXaKYUHShWDrZx3rYMvxJBZfhxbylIQSI',
+    authDomain: 'my-project-firebase-94bad.firebaseapp.com',
+    messagingSenderId: '408846258230',
+  },};
